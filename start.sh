392ddb14589c4d0d10e3708a24c506333ee5fcd6
weesauce <jacksonisace@gmail.com>
2025-11-06 12:38:07 +1100

    Resolve merge conflicts

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/shmezzy/Desktop/vrchat-whitelist
+source venv/bin/activate
+python3 bot.py
---
+++ start_bot.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d /home/shmezzy/Desktop/vrchat-whitelist
+echo Starting VRChat Discord Bot...
+python bot.py
+pause
